0113c58e8886e1a95a3429ad2384dfa44dcb8645
D:\Ordinador2 DAMVI2\SourceTree <Arnausendraf@gmail.com>
2024-11-25 23:45:23 +0100

    UF1Act16

---
+++ UF1Act16/app/src/main/java/com/example/uf1act16/MainActivity3.kt
@@ -0,0 +1,24 @@
+package com.example.uf1act16
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main3)
+
+
+        val btn: Button = findViewById(R.id.button4)
+        btn.setOnClickListener {
+            val intent: Intent= Intent(this, MainActivity:: class.java)
+            startActivity(intent)
+        }
+    }
+}
